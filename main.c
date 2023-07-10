767f47ab90daf8e19c3c9e2ac1ee3ebb6f9e4388
MikuroXina <ryosukadnak@gmail.com>
2023-07-10 16:11:21 +0900

    Fix return type

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 // x の平方根を返す関数。x >= 0 ではないとき exit(1) とする。
-int myRoot(double x) {
+double myRoot(double x) { // fix return type
     int y;
     if (x <= 0) {
         exit(1);
