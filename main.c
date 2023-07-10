0c220704c832d8b6cbf34720b86b0f389ed51c3e
MikuroXina <ryosukadnak@gmail.com>
2023-07-10 16:14:05 +0900

    Add missing includes

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
-#include <math.h>
+#include <math.h> // Add missing includes
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> // Add missing includes
 
 // x の平方根を返す関数。x >= 0 ではないとき exit(1) とする。
 double myRoot(double x) { // fix return type
